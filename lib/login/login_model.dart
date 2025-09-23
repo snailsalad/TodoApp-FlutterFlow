96cdab99f0e55d1e463d2dcacbb029e1fba4b826
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-09-23 19:26:13 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/login/login_model.dart
@@ -0,0 +1,110 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import '/index.dart';
+import 'login_widget.dart' show LoginWidget;
+import 'package:flutter/material.dart';
+
+class LoginModel extends FlutterFlowModel<LoginWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final formKey = GlobalKey<FormState>();
+  // State field(s) for TabBar widget.
+  TabController? tabBarController;
+  int get tabBarCurrentIndex =>
+      tabBarController != null ? tabBarController!.index : 0;
+  int get tabBarPreviousIndex =>
+      tabBarController != null ? tabBarController!.previousIndex : 0;
+
+  // State field(s) for SignupEmail widget.
+  FocusNode? signupEmailFocusNode;
+  TextEditingController? signupEmailTextController;
+  String? Function(BuildContext, String?)? signupEmailTextControllerValidator;
+  String? _signupEmailTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Email... is required';
+    }
+
+    if (!RegExp(kTextValidatorEmailRegex).hasMatch(val)) {
+      return 'Has to be a valid email address.';
+    }
+    return null;
+  }
+
+  // State field(s) for SignupPassword widget.
+  FocusNode? signupPasswordFocusNode;
+  TextEditingController? signupPasswordTextController;
+  late bool signupPasswordVisibility;
+  String? Function(BuildContext, String?)?
+      signupPasswordTextControllerValidator;
+  String? _signupPasswordTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Password... is required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  // State field(s) for SignUpConfirm widget.
+  FocusNode? signUpConfirmFocusNode;
+  TextEditingController? signUpConfirmTextController;
+  late bool signUpConfirmVisibility;
+  String? Function(BuildContext, String?)? signUpConfirmTextControllerValidator;
+  String? _signUpConfirmTextControllerValidator(
+      BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return 'Confirm Password... is required';
+    }
+
+    if (val.length < 7) {
+      return 'Requires at least 7 characters.';
+    }
+
+    return null;
+  }
+
+  // State field(s) for LoginEmail widget.
+  FocusNode? loginEmailFocusNode;
+  TextEditingController? loginEmailTextController;
+  String? Function(BuildContext, String?)? loginEmailTextControllerValidator;
+  // State field(s) for LoginPassword widget.
+  FocusNode? loginPasswordFocusNode;
+  TextEditingController? loginPasswordTextController;
+  late bool loginPasswordVisibility;
+  String? Function(BuildContext, String?)? loginPasswordTextControllerValidator;
+
+  @override
+  void initState(BuildContext context) {
+    signupEmailTextControllerValidator = _signupEmailTextControllerValidator;
+    signupPasswordVisibility = false;
+    signupPasswordTextControllerValidator =
+        _signupPasswordTextControllerValidator;
+    signUpConfirmVisibility = false;
+    signUpConfirmTextControllerValidator =
+        _signUpConfirmTextControllerValidator;
+    loginPasswordVisibility = false;
+  }
+
+  @override
+  void dispose() {
+    tabBarController?.dispose();
+    signupEmailFocusNode?.dispose();
+    signupEmailTextController?.dispose();
+
+    signupPasswordFocusNode?.dispose();
+    signupPasswordTextController?.dispose();
+
+    signUpConfirmFocusNode?.dispose();
+    signUpConfirmTextController?.dispose();
+
+    loginEmailFocusNode?.dispose();
+    loginEmailTextController?.dispose();
+
+    loginPasswordFocusNode?.dispose();
+    loginPasswordTextController?.dispose();
+  }
+}
